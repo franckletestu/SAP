2d5b6897a6a1c5c07d589dd48cccea4ae3964663
franckletestu <letestu.franck@gmail.com>
2022-11-02 19:45:11 +0100

    REVERT TEST

--- SAP_Asymptotique-SLite.py
+++ SAP_Asymptotique-SLite.py
@@ -15,7 +15,7 @@ import plotly.figure_factory as ff
 # ## Définition du scénario
 
 # Nombre d'antennes
-N = st.sidebar.slider('NombreS Antennes',min_value=2,max_value=64,step=1)
+N = st.sidebar.slider('Nombre Antennes',min_value=2,max_value=64,step=1)
 st.write("Nombre d'Antennes = ",N)
 
 st.latex(r''' s_i = e^{2pi \frac{d} {\lambda} sin(\theta)} ''')
