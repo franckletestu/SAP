1e6404fc3d219a763cebbb03d0f4e9bd88045566
franckletestu <letestu.franck@gmail.com>
2022-11-07 18:12:57 +0100

    debug

--- SAP_Asymptotique-SLite.py
+++ SAP_Asymptotique-SLite.py
@@ -83,7 +83,7 @@ else:
     w = Cs
     # Calcul des pondérations avec la contrainte SAP ou FF sélectionnée
     zn = np.linalg.solve(Sn, Cs)
-    #w = zn / np.dot(np.conj(Cs).T, zn)
+    w = zn / np.dot(np.conj(Cs).T, zn)
 # fin IF
 
 # TO DO
