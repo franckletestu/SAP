e10ab6548f633a48939a95295fbf992f0716571a
franckletestu <letestu.franck@gmail.com>
2022-11-07 18:06:25 +0100

    debug

--- SAP_Asymptotique-SLite.py
+++ SAP_Asymptotique-SLite.py
@@ -81,7 +81,7 @@ st.latex(r'''  z_n = S_n^{-1} * C_s ''')
 if temps_clair == True:
     # Application de la contrainte directement si absence de brouillage
     1
-    w = np.zeros((N,1))
+    w = Cs #np.zeros((N,1))
 else:
     2
     # Calcul des pondérations avec la contrainte SAP ou FF sélectionnée
